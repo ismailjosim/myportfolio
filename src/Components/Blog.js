fc75184b77d3051a82d635e54e960841518be82e
JC DESIGN <ismailjosim99@gmail.com>
2022-12-11 20:02:23 +0600

    add contact form

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SectionHeader from './utilities/SectionHeader';
 
 const Blog = () => {
     return (
-        <div>
+        <div className='w-11/12 mx-auto'>
+            <div className="min-h-screen">
+                <SectionHeader headTitle={'blog'} subTitle={'Lasted blogs'}></SectionHeader>
+                <div>
+                    <section className="flex items-center h-full p-16">
+                        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+                            <div className="text-center">
+                                <p className="text-2xl font-semibold md:text-3xl">Latest Blog will Coming Soon!</p>
+                                <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
+                                <Link to="/" className="px-8 py-3 font-semibold rounded btn-primary text-white">Back to homepage</Link>
+                            </div>
+                        </div>
+                    </section>
 
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,65 +1,91 @@
 import React from 'react';
+import SectionHeader from './utilities/SectionHeader';
+import '../styles/contact.css'
+
+
+// .contact_icon {
+//     font-size: 2rem;
+//     color: var(--first - color);
+//     margin - right: var(--mb - 0 - 75);
+// }
+
 
 const Contact = () => {
     return (
-        <section className="contact container contact" id="contact">
-            <h2 className="section_title">Contact Me</h2>
-            <span className="section_subtitle">Get in touch</span>
+        <div className='w-11/12 mx-auto'>
+            <div className="min-h-screen">
+                <SectionHeader headTitle={'Contact'} subTitle={'Get In Touch'}></SectionHeader>
+                <div className='mt-10'>
+                    <section className="contact container contact" id="contact">
+                        <div className="contact_container container grid">
+                            <div className='flex flex-col gap-10'>
+                                <div className="flex items-center gap-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                                    </svg>
 
-            <div className="contact_container container grid">
-                <div>
-                    <div className="contact_information">
-                        <i className="uil uil-phone contact_icon"></i>
-                        <div>
-                            <h3 className="contact_title">Call Me</h3>
-                            <span className="contact_subtitle">+088 0171 505 2808</span>
-                        </div>
-                    </div>
+                                    <div>
+                                        <h3 className="text-lg">Call Me</h3>
+                                        <span className="text-sm">+088 0171 505 2808</span>
+                                    </div>
+                                </div>
+                                <div className="flex items-center gap-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                    </svg>
 
-                    <div className="contact_information">
-                        <i className="uil uil-envelope contact_icon"></i>
-                        <div>
-                            <h3 className="contact_title">Email</h3>
-                            <span className="contact_subtitle">ismailjosim@yahoo.com</span>
-                        </div>
-                    </div>
+                                    <div>
+                                        <h3 className="text-lg">Email</h3>
+                                        <span className="text-sm">ismailjosim@yahoo.com</span>
+                                    </div>
+                                </div>
+                                <div className="flex items-center gap-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                    </svg>
 
-                    <div className="contact_information">
-                        <i className="uil uil-map-marker contact_icon"></i>
-                        <div>
-                            <h3 className="contact_title">Location</h3>
-                            <span className="contact_subtitle">Charfassion, Bhola, Bangladesh</span>
+
+                                    <div>
+                                        <h3 className="text-lg">Location</h3>
+                                        <span className="text-sm">Charfassion, Bhola, Bangladesh</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <form action="mailto:ismailjosim99@gmail.com" className="contact_form grid">
+                                <div className="contact_inputs grid">
+                                    <div className="contact_content">
+                                        <label htmlFor="name" className="contact_label">Name</label>
+                                        <input type="text" className="contact_input" />
+                                    </div>
+                                    <div className="contact_content">
+                                        <label htmlFor="email" className="contact_label">Email</label>
+                                        <input type="email" className="contact_input" />
+                                    </div>
+                                </div>
+                                <div className="contact_content">
+                                    <label htmlFor="project" className="contact_label">Project</label>
+                                    <input type="text" className="contact_input" />
+                                </div>
+                                <div className="contact_content">
+                                    <label htmlFor="" className="contact_label">Message</label>
+                                    <textarea name="message" id="" cols="0" rows="7" className="contact_input"></textarea>
+                                </div>
+                                <div>
+                                    <a href="mailto:ismailjosim99@gmail.com" className="button button-flex">
+                                        Send Message
+                                        <i className="uil uil-message button_icon"></i>
+                                    </a>
+                                </div>
+                            </form>
                         </div>
-                    </div>
+                    </section>
+
                 </div>
-                <form action="mailto:ismailjosim99@gmail.com" className="contact_form grid">
-                    <div className="contact_inputs grid">
-                        <div className="contact_content">
-                            <label htmlFor="name" className="contact_label">Name</label>
-                            <input type="text" className="contact_input" />
-                        </div>
-                        <div className="contact_content">
-                            <label htmlFor="email" className="contact_label">Email</label>
-                            <input type="email" className="contact_input" />
-                        </div>
-                    </div>
-                    <div className="contact_content">
-                        <label htmlFor="project" className="contact_label">Project</label>
-                        <input type="text" className="contact_input" />
-                    </div>
-                    <div className="contact_content">
-                        <label htmlFor="" className="contact_label">Message</label>
-                        <textarea name="message" id="" cols="0" rows="7" className="contact_input"></textarea>
-                    </div>
-                    <div>
-                        <a href="mailto:ismailjosim99@gmail.com" className="button button-flex">
-                            Send Message
-                            <i className="uil uil-message button_icon"></i>
-                        </a>
-                    </div>
-                </form>
+
             </div>
-        </section>
+        </div>
+
     );
 };
 
